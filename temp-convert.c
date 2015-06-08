9df0a94e580a452d7599387da774db63fe6fb11c
iohzrd <synergysymphony@gmail.com>
2015-06-07 23:32:29 -0700

    Update temp-convert.c

--- temp-convert.c
+++ temp-convert.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 
-main()
+int main()
 {
-        char corf;
-        float fahr, celsius;
+	char corf;
+	float fahr, celsius;
 
-        printf("Convert from C or F?\n");
-        scanf("%c", &corf);
-        if (corf == 'f' || corf == 'F')
-        {
-                printf("# to convert?\n");
-                scanf("%f", &fahr);
-                celsius = (5.0/9.0) * (fahr-32.0);
-                printf("%.1f F = %.1f C\n", fahr, celsius);
-        }
-        else if (corf == 'c' || corf == 'C')
-        {
-                printf("# to convert?\n");
+	printf("Convert from C or F?\n");
+	scanf("%c", &corf);
+	if (corf == 'f' || corf == 'F')
+	{
+		printf("# to convert?\n");
+		scanf("%f", &fahr);
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%.1f F = %.1f C\n", fahr, celsius);
+	}
+	else if (corf == 'c' || corf == 'C')
+	{
+		printf("# to convert?\n");
                 scanf("%f", &celsius);
                 fahr = (celsius*1.8) + 32.0;
                 printf("%.1f C = %.1f F\n", celsius, fahr);
-        }
-        return 0;
+	}
+	return 0;
 }
